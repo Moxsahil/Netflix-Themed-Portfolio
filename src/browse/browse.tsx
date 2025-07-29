97f6c3589bea4305aed41cf121b9b14790656050
Sahil Barak <94597148+Moxsahil@users.noreply.github.com>
2025-07-29 07:37:44 +0530

    Undo Git LFS tracking for .mp4 files

--- src/browse/browse.tsx
+++ src/browse/browse.tsx
@@ -25,21 +25,21 @@ const Browse: React.FC = () => {
       // backgroundGif:
       //   "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGcyYnBoM3h5aXl6OHJuN3FrMG55c3dieWZ4bDYxd2J1czU4eHBpMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/4rZA5D22301iMgrUNd/giphy.gif",
 
-      backgroundVideo: "/videos/developer.mp4",
+      backgroundVideo: "/videos/developer.webm",
     },
     {
       name: "stalker",
       image: StalkerImage,
       // backgroundGif:
       //   "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExc28yMjMyZmJ6eWtxbmNwdDV6cXk4dWZmcjFhZms2cXBjN2h5ZDJjeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QjZXUBUr89CkiWLPjL/giphy.gif",
-      backgroundVideo: "/videos/stalker.mp4",
+      backgroundVideo: "/videos/stalker.webm",
     },
     {
       name: "adventurer",
       image: adventurerImage,
       // backgroundGif:
       //   "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmxib24ycWo2cjlmazh0NGV5NTZ2Mzd2YWY0M2tvam9oYXBwYW1ocCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ERKMnDK6tkzJe8YVa3/giphy-downsized-large.gif",
-      backgroundVideo: "/videos/adventure.mp4",
+      backgroundVideo: "/videos/adventure.webm",
     },
   ];
 
